8e1dbd9aa9412545fb8a5f01281026dfe6695b96
SkylarHammond55 <skylarhammond55@gmail.com>
2023-09-11 14:22:57 -0500

    cards

--- client/src/components/Square.js
+++ client/src/components/Square.js
@@ -4,7 +4,7 @@ function Square({ chooseSquare, val }) {
   return (
     <div className="square" onClick={chooseSquare}>
       {val === "X" ? (
-        <img src="/path/to/O.png" alt="X" />
+        <img src="https://p7.hiclipart.com/preview/175/716/851/harry-potter-and-the-philosopher-s-stone-hermione-granger-clip-art-harry-potter.jpg" alt="X" />
       ) : val === "O" ? (
         <img src="/path/to/O.png" alt="O" />
       ) : null}
