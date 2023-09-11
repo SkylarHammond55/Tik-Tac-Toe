3ba37b286c1dfe1b9f3faf9767c5746f45e6767e
Johns Gaming PC <1998johngtz@gmail.com>
2023-09-11 10:35:02 -0500

    homepage login sign up moving background

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import { useState } from "react";
 import JoinGame from "./components/JoinGame";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Logo from "./components/Logo"; // Import the Logo component
 
 function App() {
   const api_key = "ufys48qg5naw";
@@ -42,19 +46,33 @@ function App() {
         setIsAuth(true);
       });
   }
-  return (
-    <div className="App">
 
-<div class="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+  
+  const particlesInit = useCallback(async engine => {      
+    await loadFull(engine);
+  }, []);
+  
+  const particlesLoaded = useCallback(async container => {
+  await console.log(container);
+  }, []);
+
+  return (
+    <div>
+
+      
+        
+
+      <div className="backg">
+    <Particles
+  id="tsparticles"
+  init={particlesInit}
+  loaded={particlesLoaded}
+  options={{ "fullScreen": false, "background":{ "image":" linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)" }, "particles":{ "number":{ "value":10, "density":{ "enable":true, "value_area":600 } }, "color":{ "value":"#ffffff" }, "shape": { "type": "square", "stroke":{ "width":0, "color":"#000000" }, "polygon":{ "nb_sides":5 } }, "opacity":{ "value":0.25, "random":true, "anim":{ "enable":false, "speed":1, "opacity_min":0.1, "sync":false } }, "size":{ "value":29, "random":true, "anim":{ "enable":false, "speed":2, "size_min":0.1, "sync":false } }, "line_linked":{ "enable":false, "distance":300, "color":"#ffffff", "opacity":0, "width":0 }, "move":{ "enable":true, "speed":0.5, "direction":"top", "straight":true, "out_mode":"out", "bounce":false, "attract":{ "enable":false, "rotateX":600, "rotateY":1200 } } }, "interactivity":{ "detect_on":"canvas", "events":{ "onhover":{ "enable":false, "mode":"repulse" }, "onclick":{ "enable":false, "mode":"push" }, "resize":true }, "modes":{ "grab":{ "distance":800, "line_linked":{ "opacity":1 } }, "bubble":{ "distance":790, "size":79, "duration":2, "opacity":0.8, "speed":3 }, "repulse":{ "distance":400, "duration":0.4 }, "push":{ "particles_nb":4 }, "remove":{ "particles_nb":2 } } }, "retina_detect":true}}
+          /></div>
+
+  <div className="wrapperhome">
+      <div className="App">
+      <div className="logoc"><Logo /></div>
 
       {isAuth ? (
         <Chat client={client}>
@@ -62,12 +80,17 @@ function App() {
           <button onClick={logOut}> Log Out</button>
         </Chat>
       ) : (
-        <>
+        <> 
           <SignUp setIsAuth={setIsAuth} />
           <Login setIsAuth={setIsAuth} />
         </>
       )}
-    </div>
+      </div>
+  </div>
+
+  </div>
+    
+  
   );
 }
 
